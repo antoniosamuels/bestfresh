e66cf027cb9a93265e1bcd7969602f2d3d57f0a1
Antonio Samuels <design@antoniosamuels.com>
2016-09-11 19:15:59 -0400

    add image

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -3,6 +3,6 @@ class Farm < ApplicationRecord
     self.genre.gsub!(/[\[\]\"]/, "") if attribute_present?("genre")
   end
 
-  has_attached_file :image, styles: { medium: "800x400>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "600x400>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
